d0f97c68a0f0dd3547a0835c8e72e0246fa2541e
sunder <alxndr.konkov@gmail.com>
2022-11-28 18:54:09 +0300

    Created test

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,39 +1,49 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
-import { PRODUCT_NOT_FOUND } from 'src/product/product.constants';
-import { CreateReviewDto } from './dto/create-review.dto';
-import { REVIEW_NOT_FOUND } from './review.constants';
-import { ReviewService } from './review.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from "@nestjs/common";
+import { PRODUCT_NOT_FOUND } from "../product/product.constants";
+import { CreateReviewDto } from "./dto/create-review.dto";
+import { REVIEW_NOT_FOUND } from "./review.constants";
+import { ReviewService } from "./review.service";
 
-@Controller('review')
+@Controller("review")
 export class ReviewController {
-    constructor(private readonly reviewService: ReviewService){}
+  constructor(private readonly reviewService: ReviewService) {}
 
-    @HttpCode(201)
-    @Post('create')
-    async create(@Body() dto: CreateReviewDto) {
-        return this.reviewService.create(dto);
+  @HttpCode(201)
+  @Post("create")
+  async create(@Body() dto: CreateReviewDto) {
+    return this.reviewService.create(dto);
+  }
+
+  @Delete(":id")
+  async delete(@Param("id") id: string) {
+    const deletedDoc = await this.reviewService.delete(id);
+
+    if (!deletedDoc) {
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
+  }
 
-    @Delete(':id')
-    async delete(@Param('id') id: string) {
-        const deletedDoc = await this.reviewService.delete(id);
+  @Get("byProduct/:productId")
+  async getByProduct(@Param("productId") productId: string) {
+    return this.reviewService.findByProductId(productId);
+  }
 
-        if(!deletedDoc) {
-            throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
-        }
+  @Delete("byProduct/:id")
+  async deleteByProductId(@Param("id") productId: string) {
+    const deletedDoc = await this.reviewService.deleteByProductId(productId);
+
+    if (!deletedDoc) {
+      throw new HttpException(PRODUCT_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
-
-    @Get('byProduct/:productId')
-    async getByProduct(@Param('productId') productId: string) {
-        return this.reviewService.findByProductId(productId);
-    }
-
-    @Delete('byProduct/:id')
-    async deleteByProductId(@Param('id') productId: string) {
-        const deletedDoc = await this.reviewService.deleteByProductId(productId);
-
-        if(!deletedDoc) {
-            throw new HttpException(PRODUCT_NOT_FOUND, HttpStatus.NOT_FOUND)
-        }
-    }
+  }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,27 +1,32 @@
-import { InjectModel } from 'nestjs-typegoose';
-import { ModelType, DocumentType } from '@typegoose/typegoose/lib/types';
-import { Types } from 'mongoose';
-import { CreateReviewDto } from './dto/create-review.dto';
-import { ReviewModel } from './review.model';
-import { Injectable } from '@nestjs/common';
+import { InjectModel } from "nestjs-typegoose";
+import { ModelType, DocumentType } from "@typegoose/typegoose/lib/types";
+import { Types } from "mongoose";
+import { CreateReviewDto } from "./dto/create-review.dto";
+import { ReviewModel } from "./review.model";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class ReviewService {
-    constructor(@InjectModel(ReviewModel) private readonly reviewModel: ModelType<ReviewModel>) {}
+  constructor(
+    @InjectModel(ReviewModel)
+    private readonly reviewModel: ModelType<ReviewModel>,
+  ) {}
 
-    async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
-        return this.reviewModel.create(dto);
-    }
+  async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
+    return this.reviewModel.create(dto);
+  }
 
-    async delete(id: string): Promise<DocumentType<ReviewModel> | null> {
-        return this.reviewModel.findByIdAndDelete(id).exec();
-    }
+  async delete(id: string): Promise<DocumentType<ReviewModel> | null> {
+    return this.reviewModel.findByIdAndDelete(id).exec();
+  }
 
-    async findByProductId(id: string): Promise<DocumentType<ReviewModel>[]> {
-        return this.reviewModel.find({ productId: new Types.ObjectId(id) }).exec();
-    }
+  async findByProductId(id: string): Promise<DocumentType<ReviewModel>[]> {
+    return this.reviewModel.find({ productId: new Types.ObjectId(id) }).exec();
+  }
 
-    async deleteByProductId(id: string) {
-        return this.reviewModel.deleteMany({ productId: new Types.ObjectId(id) }).exec();
-    }
+  async deleteByProductId(id: string) {
+    return this.reviewModel
+      .deleteMany({ productId: new Types.ObjectId(id) })
+      .exec();
+  }
 }
